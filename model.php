78907652e183b32801353d46cb098ba6bc668b71
Ivanchi990 <97800278+Ivanchi990@users.noreply.github.com>
2022-11-20 19:59:39 +0100

    Mejorando

--- model.php
+++ model.php
@@ -108,6 +108,89 @@ function buscarAero($aero1, $aero2)
 
 function showReservas()
 {
-    
+    require("conexion.php");
+
+    $user = $_SESSION["user"];
+
+    $query = "select concat(u.nombre) as Usuario, concat(v.ciudadOrigen, '-',v.aeropuertoOrigen) as Origen,
+    v.fechaSalida as FechaSalida, concat(v.ciudadDestino, '-', v.aeropuertoDestino) as Destino, v.fechaLlegada as FechaLlegada,
+    r.plazas as PlazasReservadas, v.idVuelo as idVuelo from reservas r join usuarios u join vuelos v on r.idUsuario = u.dni and r.idVuelo = v.idVuelo
+    where r.idUsuario = (SELECT dni FROM usuarios WHERE nombre = '$user')";
+
+    $result = $mysqli->query($query);
+
+    echo
+    '
+    <h1>Reservas</h1>
+    <table border="1">
+        <tr>
+            <td>Usuario reserva</td>
+            <td>Ciudad salida</td>
+            <td>Fecha salida</td>
+            <td>Ciudad llegada</td>
+            <td>Fecha llegada</td>
+            <td>Plazas reservadas</td>
+            <td>Ver mas</td>
+        </tr>
+    ';
+        while($row = $result->fetch_assoc())
+        {
+            echo'<tr>';
+                echo"<td>" .$row["Usuario"]. "</td>";
+                echo"<td>" .$row["Origen"]. "</td>";
+                echo"<td>" .$row["FechaSalida"]. "</td>";
+                echo"<td>" .$row["Destino"]. "</td>";
+                echo"<td>" .$row["FechaLlegada"]. "</td>";
+                echo"<td>" .$row["PlazasReservadas"]. "</td>";
+                echo"<td><button onClick='clickMe()'>Eliminar</button></td>";
+                echo
+                "
+                    <script type='text/javascript'>
+                        function clickMe()
+                        {
+                            var result ='<?php
+                                include('model.php');
+                                eliminarReserva(".$row["idVuelo"].");
+                            ?>';
+                        }
+                    </script>
+                ";
+            echo'</tr>';
+        }
+    echo
+    '
+        </table>
+    ';
+}
+
+function eliminarReserva($idVuelo)
+{
+    require("conexion.php");
+
+    $user = $_SESSION["user"];
+
+    $queryP = "SELECT plazas FROM reservas WHERE idUsuario = (SELECT dni FROM usuarios WHERE nombre = '$user') and idVuelo = '$idVuelo'";
+
+    $resultP = $mysqli->query($queryP);
+
+    $row = $resultP->fetch_assoc();
+
+    $antiguo = "SELECT plazas FROM vuelos where idVuelo = '$idVuelo'";
+
+    $resultA = $mysqli->query($antiguo);
+
+    $ro2 = $resultA->fetch_assoc();
+
+    $plazas = $row["plazas"] + $ro2["plazas"];
+
+    $update = "UPDATE vuelos SET plazas = '$plazas' WHERE idVuelo = '$idVuelo'";
+
+    $mysqli->query($update);
+
+    $delete = "DELETE FROM reservas WHERE idVuelo = '$idVuelo' and idUsuario = (SELECT dni FROM usuarios WHERE nombre = '$user')";
+
+    $mysqli->query($delete);
+
+    echo '<script type="text/JavaScript">location.reload();</script>';
 }
 ?>
